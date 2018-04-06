8ab53588a74b5811074d07a849813de0e80bbbb4
wanrong <zhuwr@pku.edu.cn>
2018-04-06 14:22:11 +0800

    for coco training and evaluation

--- examples/seqgan/discriminator.py
+++ examples/seqgan/discriminator.py
@@ -20,29 +20,6 @@ class Discriminator:
             self.labels = tf.placeholder(dtype=tf.int32, shape=[self.batch_size, 1], name="labels")
             self.samples = tf.placeholder(dtype=tf.int32, name="samples",
                                           shape=[self.batch_size, self.max_seq_length + 1])
-            """
-            self.embedder = tx.modules.WordEmbedder(
-                vocab_size=self.vocab_size, hparams=config.emb)
-            self.encoder = tx.modules.UnidirectionalRNNEncoder(
-                hparams={"rnn_cell": config.cell})
-
-            self.encoder_unit_num = config.cell["kwargs"]["num_units"]
-
-            emb_inputs = self.embedder(self.samples)
-            if config.keep_prob < 1:
-                emb_inputs = tf.nn.dropout(
-                    emb_inputs, tx.utils.switch_dropout(config.keep_prob))
-            enc_outputs, enc_last = self.encoder(inputs=emb_inputs)
-
-            # build classifying layer params
-            self.W = tf.Variable(tf.random_uniform([self.encoder_unit_num, class_num], -1.0, 1.0), name="W")
-            self.b = tf.Variable(tf.constant(0.1, shape=[class_num]), name="b")
-
-            # Make predictions for training
-            hidden_state = enc_last[1][0]
-            self.scores = tf.nn.xw_plus_b(hidden_state, self.W, self.b, name="scores")
-            self.ypred_for_auc = tf.nn.softmax(self.scores)
-            self.predictions = tf.argmax(self.ypred_for_auc, 1, name="predictions")"""
 
             self.classifier = Conv1DClassifier()
             self.embedder = tx.modules.WordEmbedder(
