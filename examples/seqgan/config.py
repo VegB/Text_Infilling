53cccf80256fe679d54c7b1b9dd096619ffedac1
wanrong <zhuwr@pku.edu.cn>
2018-05-02 23:42:35 +0800

    SeqGAN on server. can run

--- examples/seqgan/config.py
+++ examples/seqgan/config.py
@@ -16,17 +16,21 @@
 
 # pylint: disable=invalid-name, too-few-public-methods, missing-docstring
 
-# train_file = "../../data/small_sent.txt"
-# eval_file = "../../data/small_sent.txt"
-# vocab_file = "../../data/vocab.txt"
+'''train_file = "../../data/small_sent.txt"
+eval_file = "../../data/small_sent.txt"
+vocab_file = "../../data/vocab.txt"
+'''
 train_file = "../../data/coco.txt"
+eval_file = "../../data/coco.eval.txt"
 vocab_file = "../../data/coco_vocab.txt"
 positive_file = "./data/positive.txt"
 negative_file = "./data/negative.txt"
+log_file = "./data/log.txt"
+ckpt = "./checkpoint/"
 
 init_scale = 0.1
 num_epochs = 13
-hidden_size = 400
+hidden_size = 32
 keep_prob = 1.0
 batch_size = 20
 num_steps = 20
@@ -36,8 +40,6 @@ target_pretrain_epoch = 80
 generator_pretrain_epoch = 80
 discriminator_pretrain_epoch = 80
 adversial_epoch = 100
-adv_g_step = 1
-adv_d_epoch = 15
 
 cell = {
     "type": "LSTMBlockCell",
@@ -46,17 +48,28 @@ cell = {
         "forget_bias": 0.
     },
     "dropout": {"output_keep_prob": keep_prob},
-    "num_layers": 2
+    "num_layers": 1
 }
 emb = {
     "dim": hidden_size
 }
 opt = {
     "optimizer": {
-        "type": "MomentumOptimizer",
+        "type": "AdamOptimizer",
         "kwargs": {
-            "learning_rate": 0.01,
-            "momentum": 0.9
+            "learning_rate": 0.01
         }
     }
 }
+d_opt = { 
+    "optimizer": {
+        "type": "AdamOptimizer",
+        "kwargs": {
+            "learning_rate": 0.0001
+        }   
+    }   
+}
+cnn = {
+      "kernel_sizes": [2, 3],
+      # "num_filter": [100, 200]
+    }
