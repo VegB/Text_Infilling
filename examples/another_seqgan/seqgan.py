bb615e6fb92156b58736aa1421b1d6f48da6c87c
wanrong <zhuwr@pku.edu.cn>
2018-05-13 10:18:27 +0800

    init

---
+++ examples/another_seqgan/seqgan.py
@@ -0,0 +1,190 @@
+import tensorflow as tf
+import numpy as np
+import importlib
+from shutil import copyfile
+
+import texar as tx
+
+from generator import Generator
+from discriminator import Discriminator
+from dataloader import GenDataLoader, DisDataLoader
+from utils import print_result, store_output, pad_to_length
+
+
+config_path = "config"
+config = importlib.import_module(config_path)
+log = open(config.log_file, "w")
+
+
+def pretrain_generator(sess, generator, input_file, vocab_file, epoch_num=1):
+    print("-------------Pretrain Generator----------------")
+    dataloader = GenDataLoader(config, text_file=input_file,
+                               vocab_file=vocab_file, epoch_num=epoch_num)
+
+    while not dataloader.should_stop():
+        _, step, loss, outputs = sess.run([generator.train_op, generator.global_step,
+                                           generator.teacher_loss, generator.outputs],
+                                          feed_dict={generator.data_batch: dataloader.get_batch(),
+                                                     generator.global_step: dataloader.step,
+                                                     tx.global_mode(): tf.estimator.ModeKeys.TRAIN})
+        if step % 200 == 0:
+            print("%d: teacher_loss = %.6f, perplexity = %.6f" % (step, loss, np.exp(loss)))
+            print_result(outputs.sample_id[:config.print_num], dataloader.id2word, dataloader.max_len)
+
+
+def generate_negative_samples(sess, generator, input_file, vocab_file, dst_path):
+    print("-------------Generate Negative Samples----------------")
+    dataloader = GenDataLoader(config, text_file=input_file,
+                               vocab_file=vocab_file, epoch_num=1)
+
+    generated_outputs = []
+    while not dataloader.should_stop():
+        decode_output = sess.run(generator.generated_outputs,
+                                 feed_dict={generator.data_batch: dataloader.get_batch(),
+                                            tx.global_mode(): tf.estimator.ModeKeys.EVAL})
+        generated_outputs.extend(decode_output.sample_id)
+
+    store_output(output=generated_outputs, id2word=dataloader.id2word,
+                 data_path=dst_path, max_len=dataloader.max_len)
+    print_result(generated_outputs[:config.print_num], dataloader.id2word, dataloader.max_len)
+
+
+def train_discriminator(sess, discriminator, positive_file, negative_file, vocab_file, epoch_num):
+    print("-------------Train Discriminator----------------")
+    dataloader = DisDataLoader(config, epoch_num=epoch_num, positive_file=positive_file,
+                               negative_file=negative_file, vocab_file=vocab_file)
+
+    while not dataloader.should_stop():
+        r_ids, g_ids = dataloader.get_batch()
+
+        _, step, loss = sess.run([discriminator.train_op, discriminator.global_step,
+                                  discriminator.total_loss],
+                                 feed_dict={discriminator.real_samples: r_ids,
+                                            discriminator.gen_samples: g_ids,
+                                            discriminator.global_step: dataloader.step,
+                                            tx.global_mode(): tf.estimator.ModeKeys.TRAIN})
+        if step % 200 == 0:
+            print("%d: dis_total_loss: %.6f" % (step, loss))
+
+
+def update_generator(sess, generator, discriminator, positive_file, negative_file, vocab_file):
+    print("-------------Update Generator----------------")
+    dataloader = DisDataLoader(config, epoch_num=1, positive_file=positive_file,
+                               negative_file=negative_file, vocab_file=vocab_file)
+
+    gen_dataloader = GenDataLoader(config, text_file=positive_file,
+                                   vocab_file=vocab_file, epoch_num=1)
+
+    for i in range(config.g_update_batch):
+        # Teacher forcing
+        _, teacher_loss = sess.run([generator.train_op, generator.teacher_loss],
+                                   feed_dict={generator.data_batch: gen_dataloader.get_batch(),
+                                              generator.global_step: gen_dataloader.step,
+                                              tx.global_mode(): tf.estimator.ModeKeys.TRAIN})
+
+        gen_data = sess.run(generator.generated_outputs,
+                            feed_dict={tx.global_mode(): tf.estimator.ModeKeys.EVAL})
+        g_data = [pad_to_length(sent, bos=dataloader.bos_id, eos=dataloader.eos_id,
+                                pad=dataloader.pad_id, max_len=dataloader.max_len)
+                  for sent in gen_data.sample_id]
+
+        r_ids, _ = dataloader.get_batch()
+
+        _, g_preds = sess.run([discriminator.r_preds, discriminator.g_preds],
+                              feed_dict={discriminator.real_samples: r_ids,
+                                         discriminator.gen_samples: [line[1:] for line in g_data],
+                                         discriminator.global_step: dataloader.step,
+                                         tx.global_mode(): tf.estimator.ModeKeys.TRAIN})
+
+        _, update_loss = sess.run([generator.update_op, generator.update_loss],
+                                  feed_dict={generator.data_batch: g_data,
+                                             generator.rewards: [preds[:-1] for preds in g_preds],
+                                             tx.global_mode(): tf.estimator.ModeKeys.TRAIN})
+        print("%d: teacher_loss = %.6f, update_total_loss = %.6f" % (i, teacher_loss, update_loss))
+
+
+def calculate_nll(sess, generator, epoch_id, mode):
+    # NLL Oracle
+    dataloader = GenDataLoader(config, text_file=config.train_file,
+                               vocab_file=config.vocab_file, epoch_num=1)
+    nll = []
+    for i in range(50):
+        loss = sess.run([generator.teacher_loss],
+                        feed_dict={generator.data_batch: dataloader.get_batch(),
+                                   tx.global_mode(): tf.estimator.ModeKeys.TRAIN})
+        nll.append(loss)
+    nll_oracle = np.mean(nll)
+    print("%s epoch %d: nll_oracle = %f, perplexity_oracle = %f" % (mode, epoch_id, nll_oracle, np.exp(nll_oracle)))
+    log.write("%s epoch %d: perplexity_oracle = %f\n" % (mode, epoch_id, np.exp(nll_oracle)))
+
+    # NLL Gen
+    dataloader = GenDataLoader(config, text_file=config.negative_file,
+                               vocab_file=config.vocab_file, epoch_num=1)
+    nll = []
+    for i in range(50):
+        loss = sess.run([generator.teacher_loss],
+                        feed_dict={generator.data_batch: dataloader.get_batch(),
+                                   tx.global_mode(): tf.estimator.ModeKeys.TRAIN})
+        nll.append(loss)
+    nll_gen = np.mean(nll)
+    print("%s epoch %d: nll_gen = %f, perplexity_gen = %f" % (mode, epoch_id, nll_gen, np.exp(nll_gen)))
+    log.write("%s epoch %d: perplexity_gen = %f\n" % (mode, epoch_id, np.exp(nll_gen)))
+
+    # NLL Test
+    dataloader = GenDataLoader(config, text_file=config.test_file,
+                               vocab_file=config.vocab_file, epoch_num=1)
+    nll = []
+    for i in range(50):
+        loss = sess.run([generator.teacher_loss],
+                        feed_dict={generator.data_batch: dataloader.get_batch(),
+                                   tx.global_mode(): tf.estimator.ModeKeys.TRAIN})
+        nll.append(loss)
+    nll_gen = np.mean(nll)
+    print("%s epoch %d: nll_test = %f, perplexity_test = %f" % (mode, epoch_id, nll_gen, np.exp(nll_gen)))
+    log.write("%s epoch %d: perplexity_test = %f\n" % (mode, epoch_id, np.exp(nll_gen)))
+
+
+if __name__ == "__main__":
+    dataloader = GenDataLoader(config, text_file=config.train_file,
+                               vocab_file=config.vocab_file, epoch_num=1)
+    generator = Generator(config, word2id=dataloader.word2id, bos=dataloader.bos_id,
+                          eos=dataloader.eos_id, pad=dataloader.pad_id)
+    discriminator = Discriminator(config, word2id=dataloader.word2id)
+    saver = tf.train.Saver()
+
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        sess.run(tf.local_variables_initializer())
+        sess.run(tf.tables_initializer())
+        
+        generate_negative_samples(sess, generator, config.train_file, config.vocab_file,
+                                  dst_path="./data/0.txt")
+
+        for pre_epoch in range(1, config.generator_pretrain_epoch + 1):
+            pretrain_generator(sess, generator, config.train_file, config.vocab_file)
+            generate_negative_samples(sess, generator, config.train_file, config.vocab_file,
+                                      dst_path=config.negative_file)
+            calculate_nll(sess, generator, epoch_id=pre_epoch, mode="Pretrain")
+            if pre_epoch % 10 == 0:
+                train_rst_file = "./data/%d.txt" % pre_epoch
+                copyfile(config.negative_file, train_rst_file)
+                saver.save(sess, config.ckpt, global_step=pre_epoch)
+
+        train_discriminator(sess, discriminator, positive_file=config.train_file,
+                            negative_file=config.negative_file, vocab_file=config.vocab_file,
+                            epoch_num=config.discriminator_pretrain_epoch)
+        
+        for update_epoch in range(1, config.adversial_epoch + 1):
+            update_generator(sess, generator, discriminator, positive_file=config.train_file,
+                             negative_file=config.negative_file, vocab_file=config.vocab_file)
+            generate_negative_samples(sess, generator, input_file=config.train_file,
+                                      vocab_file=config.vocab_file, dst_path=config.negative_file)
+            calculate_nll(sess, generator, epoch_id=update_epoch, mode="Adversarial")
+            train_discriminator(sess, discriminator, positive_file=config.train_file,
+                                negative_file=config.negative_file, vocab_file=config.vocab_file,
+                                epoch_num=1)
+            if update_epoch % 10 == 0:
+                current_epoch = update_epoch + config.generator_pretrain_epoch
+                train_rst_file = "./data/%d.txt" % current_epoch
+                copyfile(config.negative_file, train_rst_file)
+                saver.save(sess, config.ckpt, global_step=current_epoch)
---
+++ examples/another_seqgan/utils.py
@@ -0,0 +1,70 @@
+import numpy as np
+
+
+def print_result(output, id2word, max_len):
+    for sent in output:
+        words = [id2word[id] for id in sent[:max_len]]
+        print(" ".join(words).encode("utf-8"))
+
+
+def store_output(output, id2word, data_path, max_len):
+    print("------------------ STORING OUTPUT -----------------")
+    print("len of output: %d" % len(output))
+    with open(data_path, 'wb') as fout:
+        for sent in output:
+            words = [id2word[id] for id in sent[:max_len]]
+            rst = " ".join(words) + "\n"
+            fout.write(rst.encode('utf-8'))
+
+
+def pad_to_length(content, max_len, pad, bos=None, eos=None):
+    """
+    Pad sentence with <BOS>, <EOS>, <PAD>.
+    if self.max_len = 3,
+        if bos = <BOS>, then
+            (a, b) -> (<BOS>, a, b, <EOS>, <PAD>)
+        if bos is None, then
+            (a, b) -> (a, b, <EOS>, <PAD>)
+    :param content:
+    :param max_len:
+    :param eos:
+    :param pad:
+    :param bos:
+    :return:
+    """
+    is_np_array = False
+    if isinstance(content, np.ndarray):
+        content = content.tolist()
+        is_np_array = True
+    rst = content + [eos] + [pad] * max_len
+    if bos is not None:
+        rst = [bos] + rst
+        rtn = rst[: max_len + 2]
+    else:
+        rtn = rst[: max_len + 1]
+    if is_np_array:
+        return np.array(rtn)
+    return rtn
+
+
+def sent_to_ids(words, word2id, unk_id):
+    """
+
+    :param words:
+    :param word2id:
+    :param unk_id:
+    :return:
+    """
+    return [word2id.get(word, unk_id) for word in words]
+
+
+def split_nll(filepath, dstpath):
+    with open(filepath, 'r') as fin:
+        data = fin.readlines()
+        data = [line.strip().split()[-1] for line in data]
+    with open(dstpath, 'w') as fout:
+        fout.write('oracle:\n' + '\n'.join(data[::2]) + '\n------------\ngen:\n')
+        fout.write('\n'.join(data[1::2]))
+
+
+# split_nll("./data/log.txt", "./data/nll.txt")
