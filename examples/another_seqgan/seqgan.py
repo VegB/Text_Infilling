3b64b812bff8cd884ab8c8fe457202d7f257e59e
ZhiliangTian <tianzhilianghit@gmail.com>
2018-05-15 13:04:16 +0800

    update with hzt optimizer

--- examples/another_seqgan/seqgan.py
+++ examples/another_seqgan/seqgan.py
@@ -128,6 +128,7 @@ def update_generator(sess, generator, discriminator, gen_dataloader, dis_dataloa
         _, update_loss = sess.run([generator.update_op, generator.update_loss],
                                   feed_dict={generator.data_batch: g_data,
                                              generator.rewards: [preds[:-1] for preds in g_preds],
+                                             generator.learning_rate: opt_vars['learning_rate'],
                                              tx.global_mode(): tf.estimator.ModeKeys.TRAIN})
 
     return np.exp(loss / iters)
@@ -178,16 +179,18 @@ if __name__ == "__main__":
         sess.run(tf.local_variables_initializer())
         sess.run(tf.tables_initializer())
 
-        for pre_epoch in range(1, config.generator_pretrain_epoch + 1):
+        '''for pre_epoch in range(1, config.generator_pretrain_epoch + 1):
             train_ppl = pretrain_generator(sess, generator, gen_dataloader, valid_dataloader)
             record_ppl(sess, generator, valid_dataloader, test_dataloader,
                        epoch_id=pre_epoch, train_ppl=train_ppl)
             if pre_epoch % 10 == 0:
                 saver.save(sess, config.ckpt, global_step=pre_epoch)
-
+   
         generate_negative_samples(sess, generator, gen_dataloader, dst_path=config.negative_file)
-
+        '''
+        saver.restore(sess, config.ckpt+"-80")
         train_discriminator(sess, discriminator, epoch_num=config.discriminator_pretrain_epoch)
+        saver.save(sess, 'checkpoint/pretrained/ckpt', global_step=80)
 
         for update_epoch in range(1, config.adversial_epoch + 1):
             train_ppl = update_generator(sess, generator, discriminator, gen_dataloader, dis_dataloader)
