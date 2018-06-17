544f429e28365b96fd31588e39c5c64bf6d9ef9b
wanrong <zhuwr@pku.edu.cn>
2018-06-17 14:49:19 +0800

    simplified

---
+++ examples/seqgan5/config.py
@@ -0,0 +1,111 @@
+generator_pretrain_epoch = 120
+discriminator_pretrain_epoch = 80
+adversial_epoch = 100
+hidden_size = 256
+batch_size = 64
+num_steps = 64  # ???
+embed_dim = 256
+
+dec_keep_prob_in = 0.5
+dec_keep_prob_out = 0.5
+enc_keep_prob_in = 1.0
+enc_keep_prob_out = 1.0
+
+latent_dims = 32
+
+lr_hparams = {
+    'init_lr': 0.003,
+    'update_init_lr': 0.0003,
+    'update_lr': 0.00003,
+    'decay_rate': 0.1
+}
+
+decoder_hparams = {
+    "type": "lstm"
+}
+
+enc_cell_hparams = {
+    "type": "LSTMBlockCell",
+    "kwargs": {
+        "num_units": hidden_size,
+        "forget_bias": 0.
+    },
+    "dropout": {"output_keep_prob": enc_keep_prob_out},
+    "num_layers": 1
+}
+
+dec_cell_hparams = {
+    "type": "LSTMBlockCell",
+    "kwargs": {
+        "num_units": hidden_size,
+        "forget_bias": 0.
+    },
+    "dropout": {"output_keep_prob": dec_keep_prob_out},
+    "num_layers": 1
+}
+
+emb_hparams = {
+    'name': 'lookup_table',
+    "dim": embed_dim,
+    'initializer' : {
+        'type': 'random_normal_initializer',
+        'kwargs': {
+            'mean': 0.0,
+            'stddev': embed_dim**-0.5,
+        },
+    }
+}
+
+
+# KL annealing
+kl_anneal_hparams={
+    "warm_up": 10,
+    "start": 0.01
+}
+
+train_data_hparams = {
+    "num_epochs": 1,
+    "batch_size": batch_size,
+    "seed": 123,
+    "dataset": {
+        "files": 'ptb_data/ptb.train.txt',
+        "vocab_file": 'ptb_data/vocab.txt'
+    }
+}
+
+val_data_hparams = {
+    "num_epochs": 1,
+    "batch_size": batch_size,
+    "seed": 123,
+    "dataset": {
+        "files": 'ptb_data/ptb.valid.txt',
+        "vocab_file": 'ptb_data/vocab.txt'
+    }
+}
+
+test_data_hparams = {
+    "num_epochs": 1,
+    "batch_size": batch_size,
+    "dataset": {
+        "files": 'ptb_data/ptb.test.txt',
+        "vocab_file": 'ptb_data/vocab.txt'
+    }
+}
+
+d_opt_hparams = {
+    "optimizer": {
+        "type": "AdamOptimizer",
+        "kwargs": {
+            "learning_rate": 0.001
+        }
+    }
+}
+
+update_opt_hparams = {
+    "optimizer": {
+        "type": "AdamOptimizer",
+        "kwargs": {
+            "learning_rate": 0.001
+        }
+    }
+}
---
+++ examples/seqgan5/seqgan_train.py
@@ -0,0 +1,276 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+# pylint: disable=invalid-name, no-member, too-many-locals
+
+import os
+import sys
+import time
+import importlib
+import numpy as np
+import tensorflow as tf
+import texar as tx
+
+flags = tf.flags
+
+flags.DEFINE_string("data_path", "./", "Directory containing PTB. If not exists, "
+                    "the directory will be created, and the data will be downloaded.")
+flags.DEFINE_string("config", "config", "The config to use.")
+FLAGS = flags.FLAGS
+
+config = importlib.import_module(FLAGS.config)
+
+def prepare_data(train_path):
+    """Download the PTB or Yahoo dataset
+    """
+    ptb_url = 'https://jxhe.github.io/download/ptb_data.tgz'
+
+    data_path = FLAGS.data_path
+
+    if not tf.gfile.Exists(train_path):
+        url = ptb_url
+        tx.data.maybe_download(url, data_path, extract=True)
+        os.remove('%s_data.tgz' % FLAGS.dataset)
+
+        data_path = os.path.join(data_path, '%s_data' % FLAGS.dataset)
+
+        train_path = os.path.join(data_path, "%s.train.txt" % FLAGS.dataset)
+        valid_path = os.path.join(data_path, "%s.valid.txt" % FLAGS.dataset)
+        test_path = os.path.join(data_path, "%s.test.txt" % FLAGS.dataset)
+        vocab_path = os.path.join(data_path, "vocab.txt")
+
+        config.train_data_hparams['dataset'] = {'files': train_path,
+                                                'vocab_file': vocab_path}
+        config.val_data_hparams['dataset'] = {'files': valid_path,
+                                              'vocab_file': vocab_path}
+        config.test_data_hparams['dataset'] = {'files': test_path,
+                                               'vocab_file': vocab_path}
+
+
+def _main(_):
+    prepare_data(config.train_data_hparams['dataset']['files'])
+
+    # Data
+    train_data = tx.data.MonoTextData(config.train_data_hparams)
+    val_data = tx.data.MonoTextData(config.val_data_hparams)
+    test_data = tx.data.MonoTextData(config.test_data_hparams)
+    iterator = tx.data.TrainTestDataIterator(train=train_data,
+                                             val=val_data,
+                                             test=test_data)
+    data_batch = iterator.get_next()
+
+    batch_size = config.batch_size
+    num_steps = config.num_steps
+    vocab_size = train_data.vocab.size
+    opt_vars = {
+        'learning_rate': config.lr_hparams['init_lr'],
+        'update_learning_rate': config.lr_hparams['update_lr'],
+        'best_valid_ppl': 1e100,
+        'steps_not_improved': 0
+    }
+
+    # Model architecture
+    g_embedder = tx.modules.WordEmbedder(vocab_size=vocab_size, hparams=config.emb_hparams)
+    input_embed = g_embedder(data_batch["text_ids"][:, :-1])
+
+    if config.enc_keep_prob_in < 1:
+        input_embed = tf.nn.dropout(
+            input_embed, tx.utils.switch_dropout(config.enc_keep_prob_in))
+
+    decoder = tx.modules.BasicRNNDecoder(vocab_size=vocab_size,
+                                         hparams={"rnn_cell": config.dec_cell_hparams})
+    initial_state = decoder.zero_state(batch_size=batch_size, dtype=tf.float32)
+
+    # ------------Pretrain Generator---------------
+    # input_embed.set_shape((batch_size, num_steps, config.embed_dim))
+    outputs, _, _ = decoder(
+        initial_state=initial_state,
+        decoding_strategy="train_greedy",
+        inputs=input_embed,
+        sequence_length=data_batch["length"] - 1)
+
+    mle_loss = tx.losses.sequence_sparse_softmax_cross_entropy(
+        labels=data_batch["text_ids"][:, 1:],
+        logits=outputs.logits,
+        sequence_length=data_batch["length"] - 1)
+
+    global_step = tf.Variable(0, dtype=tf.int32)
+    learning_rate = tf.placeholder(dtype=tf.float32, shape=(), name='learning_rate')
+    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate,
+                                       beta1=0., beta2=0.999, epsilon=1e-9)
+    gen_train_op = optimizer.minimize(mle_loss, global_step=global_step)
+
+    # -------------Generator Infer-------------------
+    start_tokens = tf.cast(data_batch["text_ids"][:, 0], dtype=tf.int32)
+    start_tokens.set_shape((batch_size))
+    infer_outputs, _, sequence_length = decoder(
+        decoding_strategy="infer_sample",
+        start_tokens=start_tokens,
+        end_token=train_data.vocab.eos_token_id,
+        embedding=g_embedder,
+        initial_state=initial_state,
+        max_decoding_length=num_steps)
+
+    infer_logits = infer_outputs.logits
+    infer_sample_ids = infer_outputs.sample_id
+
+    # ------------Pretrain Discriminator---------------
+    discriminator = tx.modules.UnidirectionalRNNClassifier(
+        hparams={"clas_strategy": "time_wise", "num_classes": 1})
+    d_embedder = tx.modules.WordEmbedder(vocab_size=vocab_size, hparams=config.emb_hparams)
+
+    r_logits, _ = discriminator(d_embedder(data_batch["text_ids"]))
+    f_logits, _ = discriminator(d_embedder(infer_sample_ids))
+
+    r_loss = tx.losses.sequence_sigmoid_cross_entropy(
+        labels=tf.ones_like(data_batch["text_ids"], dtype=tf.float32),
+        logits=tf.squeeze(r_logits),
+        sequence_length=data_batch["length"])  # r_preds -> 1.
+    f_loss = tx.losses.sequence_sigmoid_cross_entropy(
+        labels=tf.zeros_like(infer_sample_ids, dtype=tf.float32),
+        logits=tf.squeeze(f_logits),
+        sequence_length=sequence_length)  # infer_logits -> 0.
+    dis_loss = r_loss + f_loss
+    dis_loss.set_shape(())
+
+    dis_train_op = tx.core.get_train_op(dis_loss, global_step=global_step,
+                                        increment_global_step=False, hparams=config.d_opt_hparams)
+
+    # ------------Adeversarial---------------
+    infer_logits = \
+        tf.clip_by_value(tf.nn.softmax(infer_logits) * tf.one_hot(infer_sample_ids, vocab_size), 1e-20, 1)
+
+    expected_reward = tf.Variable(tf.zeros((num_steps,)))  # (num_step,), exp_reward at each step
+    reward = tf.squeeze(f_logits) - expected_reward[:tf.shape(f_logits)[1]]
+    mean_reward = tf.reduce_mean(reward)
+    exp_reward_loss = tf.reduce_mean(tf.abs(reward))
+    exp_reward_loss.set_shape(())
+    exp_op = tx.core.get_train_op(exp_reward_loss, global_step=global_step,
+                                  increment_global_step=False, hparams=config.update_opt_hparams)
+
+    reward = tx.losses.discount_reward(reward, sequence_length=tf.squeeze(sequence_length), tensor_rank=2)
+    update_loss = -tf.reduce_mean(tf.log(infer_logits) * tf.expand_dims(reward, -1))
+    update_optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate,
+                                              beta1=0., beta2=0.999, epsilon=1e-9)
+    gen_op = update_optimizer.minimize(update_loss, global_step=global_step)
+    update_op = tf.group(gen_op, exp_op)
+
+    def _g_run_epoch(sess, mode_string):
+        if mode_string in ['train', 'update']:
+            iterator.switch_to_train_data(sess)
+        elif mode_string == 'valid':
+            iterator.switch_to_val_data(sess)
+        elif mode_string == 'test':
+            iterator.switch_to_test_data(sess)
+        else:
+            sys.exit("INVALID MODE %s, expecting one of "
+                     "['train', 'valid', 'test', 'update']" % mode_string)
+
+        if mode_string == 'update':
+            fetches = {
+                "mean_rwd": mean_reward,
+                "exp_rwd_loss": exp_reward_loss,
+                "update_loss": update_loss,
+                "train_op": update_op,
+                "exp_rwd": expected_reward,
+                'step': global_step
+            }
+        else:
+            fetches = {
+                "mle_loss": mle_loss,
+                'step': global_step
+            }
+            if mode_string == 'train':
+                fetches["train_op"] = gen_train_op
+
+        loss, iters = 0., 0
+
+        while True:
+            try:
+                feed_dict = {
+                    learning_rate: opt_vars['learning_rate'],
+                    tx.global_mode(): tf.estimator.ModeKeys.TRAIN if mode_string in ['train', 'update']
+                    else tf.estimator.ModeKeys.EVAL
+                }
+
+                rtns = sess.run(fetches, feed_dict)
+                iters += num_steps
+                ppl = np.exp(loss / iters)
+
+                if mode_string != 'update':
+                    loss += rtns["mle_loss"]
+
+                if mode_string in ['train', 'update'] and rtns['step'] % 100 == 0:
+                    valid_ppl = _g_run_epoch(sess, 'valid')
+                    test_ppl = _g_run_epoch(sess, 'test')
+                    if mode_string == 'train':
+                        rst = "step: %d, v_ppl: %.6f, tst_ppl: %.6f, tr_ppl: %.6f, lr: %.7f" % \
+                              (rtns['step'], valid_ppl, test_ppl, ppl, opt_vars['learning_rate'])
+                    else:
+                        rst = "step: %d, v_ppl: %.6f, tst_ppl: %.6f, mean_rwd: %.6f, exp_rwd_loss:" \
+                              " %.6f, update_loss: %.6f" % (rtns['step'], valid_ppl, test_ppl,
+                                                            rtns['mean_rwd'], rtns['exp_rwd_loss'], rtns['update_loss'])
+                    print_and_write_to_file(rst, eval_log)
+
+                    if valid_ppl < opt_vars['best_valid_ppl']:
+                        opt_vars['best_valid_ppl'] = valid_ppl
+                        opt_vars['steps_not_improved'] = 0
+                    else:
+                        opt_vars['steps_not_improved'] += 1
+
+                    if opt_vars['steps_not_improved'] >= 30:
+                        opt_vars['steps_not_improved'] = 0
+                        opt_vars['learning_rate'] *= config.lr_hparams['lr_decay']
+
+            except tf.errors.OutOfRangeError:
+                ppl = np.exp(loss / iters)
+                break
+
+        return ppl
+
+    def _d_run_epoch(sess):
+        fetches = {
+            "mle_loss": dis_loss,
+            "r_loss": r_loss,
+            "f_loss": f_loss,
+            "train_op": dis_train_op
+        }
+        step = 0
+        while True:
+            try:
+                feed_dict = {tx.global_mode(): tf.estimator.ModeKeys.TRAIN}
+                rtns = sess.run(fetches, feed_dict)
+                if step % 200 == 0:
+                    print("{0:3d}: dis_total_loss: {1:6f}, r_loss: {2:6f}, f_loss: {3:6f}"
+                          .format(step, rtns['mle_loss'], rtns['r_loss'], rtns['f_loss']))
+                step += 1
+            except tf.errors.OutOfRangeError:
+                break
+
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        sess.run(tf.local_variables_initializer())
+        sess.run(tf.tables_initializer())
+
+        saver = tf.train.Saver()
+
+        for g_epoch in range(config.generator_pretrain_epoch):
+            _g_run_epoch(sess, 'train')
+            if (g_epoch + 1) % 20 == 0:
+                saver.save(sess, config.log_hparams['ckpt'], global_step=g_epoch + 1)
+
+        for d_epoch in range(config.discriminator_pretrain_epoch):
+            _d_run_epoch(sess)
+        saver.save(sess, config.log_hparams['ckpt'], global_step=config.training_hparams['generator_pretrain_epoch'] + 1)
+
+        opt_vars['learning_rate'] = config.lr_hparams['update_init_lr']
+        for update_epoch in range(config.adversial_epoch):
+            _g_run_epoch(sess, 'update')
+            if (update_epoch + 1) % 20 == 0:
+                saver.save(sess, config.log_hparams['ckpt'],
+                           global_step=config.training_hparams['generator_pretrain_epoch'] + update_epoch + 1)
+
+
+if __name__ == '__main__':
+    tf.app.run(main=_main)
