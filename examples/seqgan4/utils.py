440845f1349f8b01f95f773f4dbffe3e25d0ffb2
VegB <zhuwr@pku.edu.cn>
2018-08-16 17:11:25 -0400

    merge

--- examples/seqgan4/utils.py
+++
@@ -1,5 +0,0 @@
-def print_and_write_to_file(rst, fout, print_out=True):
-    if print_out:
-        print(rst)
-    fout.write(rst + '\n')
-    fout.flush()
--- examples/seqgan5/run.sh
+++
@@ -1,8 +0,0 @@
-rm ptb.*
-rm -rf ptb_log.*
-python data_utils.py --config config_ptb_small
-python data_utils.py --config config_ptb_medium
-python data_utils.py --config config_ptb_large
-CUDA_VISIBLE_DEVICES="0" nohup python seqgan_train.py --config config_ptb_small --dataset ptb > ptb.small.nohup.out &
-CUDA_VISIBLE_DEVICES="1" nohup python seqgan_train.py --config config_ptb_medium --dataset ptb > ptb.medium.nohup.out &
-CUDA_VISIBLE_DEVICES="2" nohup python seqgan_train.py --config config_ptb_large --dataset ptb > ptb.large.nohup.out &
