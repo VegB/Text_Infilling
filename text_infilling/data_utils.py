c96a95ee8068d000296db8ace58613792515afe4
VegB <zhuwr@pku.edu.cn>
2019-01-02 19:18:28 +0800

    can download nba_lm_data, grimm_prep_data, grimm_lm_data

--- text_infilling/data_utils.py
+++ text_infilling/data_utils.py
@@ -1,5 +1,17 @@
 import tensorflow as tf
 import numpy as np
+import texar as tx
+import argparse
+import os
+
+parser = argparse.ArgumentParser(description='prepare data')
+parser.add_argument('--dataset', type=str, default='grimm_prep',
+                    help='dataset to prepare')
+parser.add_argument('--data_path', type=str, default='./',
+                    help="Directory containing coco. If not exists, "
+                    "the directory will be created, and the data "
+                    "will be downloaded.")
+args = parser.parse_args()
 
 FLAGS = tf.app.flags.FLAGS
 
@@ -87,3 +99,26 @@ def transform_input_with_is_missing_token(inputs, targets_present, mask_id):
     transformed_input = tf.where(tf.equal(inputs_present, tf.ones_like(inputs)),
                                  inputs, input_missing)
     return transformed_input
+
+
+def prepare_data(dataset):
+    """Downloads the PTB or COCO dataset
+    """
+    url = {
+        'grimm_prep': 'https://VegB.github.io/downloads/grimm_prep.tgz',
+        'grimm_lm': 'https://VegB.github.io/downloads/grimm_lm.tgz',
+        'nba_lm': 'https://VegB.github.io/downloads/nba_lm.tgz'
+    }
+    data_path = {
+        'grimm_prep': 'grimm_prep_data/',
+        'grimm_lm': 'grimm_lm_data/',
+        'nba_lm': 'nba_lm_data/'
+    }
+
+    if not tf.gfile.Exists(data_path[dataset]):
+        tx.data.maybe_download(url[dataset], args.data_path, extract=True)
+        os.remove(url[dataset].split('/')[-1])
+
+
+if __name__ == '__main__':
+    prepare_data(args.dataset)
